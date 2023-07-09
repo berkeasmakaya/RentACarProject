b10d9402cc03115877003c31fa4f5e45bd365914
Berke <asmakayab@gmail.com>
2023-07-09 13:21:09 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.Write(car.Description);
+                Console.Write("-");
+                Console.Write(car.ModelYear);
+				Console.Write("-");
+				Console.Write(car.DailyPrice + "$");
+                Console.WriteLine();
+            }
+
+        }
+	}
+}
